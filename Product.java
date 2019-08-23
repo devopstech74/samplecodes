bfca2611b7a2ad067f2dec88c74edf34f0ac1053
devopstech74 <53807957+devopstech74@users.noreply.github.com>
2019-08-23 12:38:21 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,27 @@
+package springproject.springbackend;
+
+public class Product {
+	private  int productid;
+	private String productName;
+	private float price;
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+
+}
