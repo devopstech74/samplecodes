2a376922bd02c917ebadc9d5ce3e6a5e69384513
devopstech74 <53807957+devopstech74@users.noreply.github.com>
2019-09-03 14:13:02 +0530

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+        public String getHome()
+	{
+		return "home";
+	}
+	@RequestMapping("product")
+	public String getProduct()
+	{
+		return "Product";
+	}
+}
